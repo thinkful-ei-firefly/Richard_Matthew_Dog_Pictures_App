a8895f8aca0a6733d6928e0041e6700ac0c37149
Matthew Mbonu <mcmbonu1@gmail.com>
2019-07-18 12:04:12 -0400

    steps 1 and 2

---
+++ index.js
@@ -0,0 +1,23 @@
+const getPics = num => {
+  fetch(`https://dog.ceo/api/breeds/image/random/${num}`, { mode: "cors" })
+    .then(res => res.json())
+    .then(resJson => displayPhotos(resJson.message))
+    .catch(err => console.log(err));
+};
+
+const displayPhotos = arr => {
+  const images = arr.map(url => mapUrlToImg(url)).join("");
+  const section = document.querySelector(".dog-pics");
+  section.innerHTML = images;
+};
+
+const mapUrlToImg = url => `<img src="${url}" alt="dog pic">`;
+
+const main = () => {
+  $("main").on("submit", ".form", e => {
+    e.preventDefault();
+    const num = $(".input").val();
+    getPics(num);
+  });
+};
+$(main);
